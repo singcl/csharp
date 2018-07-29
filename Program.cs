aac98e505b6e2544854b4c28877e4a93e56f5075
singcl <24661881@qq.com>
2018-07-29 19:15:56 +0800

    :art: c# project generated using dotnet new console with vscode.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld {
+    class Program {
+        static void Main (string[] args) {
+            Console.WriteLine ("Hello World");
+            Console.WriteLine ("\n What is your name?");
+            var name = Console.ReadLine ();
+            var date = DateTime.Now;
+            Console.Write ($"\nHello {name}, on {date:d} at {date:t}!");
+
+            Console.WriteLine ("\nPress any key to countine...");
+            Console.ReadKey (true);
+        }
+    }
+}
